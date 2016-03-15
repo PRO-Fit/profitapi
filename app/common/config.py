2e56122b9660c3122e1c21530d4f5000bf0f536a
jainikvora <jainik.vora231@gmail.com>
2016-03-14 23:13:53 -0700

    adding aws rds credentials

--- app/common/config.py
+++ app/common/config.py
@@ -1,7 +1,15 @@
+# DB_CONFIG = {
+#     'user': 'root',
+#     'password': 'admin',
+#     'host': 'localhost',
+#     'database': 'profit',
+#     'raise_on_warnings': True
+# }
+
 DB_CONFIG = {
     'user': 'root',
-    'password': 'admin',
-    'host': 'localhost',
+    'password': 'pr0f1tadmin',
+    'host': 'profit.cfmxakdibe3m.us-west-2.rds.amazonaws.com',
     'database': 'profit',
     'raise_on_warnings': True
 }
