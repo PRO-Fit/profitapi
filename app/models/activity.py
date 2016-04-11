22b03131250ca8ecad76831479c95a6da107d308
jainikvora <jainik.vora231@gmail.com>
2016-04-10 22:05:43 -0700

    added calories burnt calculation script, supporting method for session recommendation

--- app/models/activity.py
+++ app/models/activity.py
@@ -12,3 +12,19 @@ class Activity(object):
     def get_activities_since(activity_id):
         query = """SELECT id, user_id, start_datetime FROM t_user_activity WHERE id > %s ORDER BY id ASC""" % activity_id
         return Db.execute_select_query(query)
+
+    @staticmethod
+    def get_activities_since_with_user_bio(activity_id):
+        query = """SELECT tau.id, tau.user_id, tau.workout_type_id, distance, tu.weight, tu.height, tu.dob
+                    FROM t_user_activity tau
+                    INNER JOIN t_user tu ON (tau.user_id = tu.user_id)
+                    WHERE tau.id > %s
+                    ORDER BY tau.id ASC""" % activity_id
+        return Db.execute_select_query(query)
+
+    @staticmethod
+    def update_activity_calories_burnt(activity_id, calories_burnt):
+        query = """UPDATE t_user_activity SET
+                    calories_burnt = %s
+                   WHERE id = %s"""
+        return Db.execute_update_query(query % (calories_burnt, activity_id))
--- app/scripts/map_activity_session_to_goal.py
+++ app/scripts/map_activity_session_to_goal.py
@@ -2,6 +2,7 @@ import os.path
 
 from app import app
 from app.models.activity import Activity
+from app.scripts.calculate_activity_calories_burnt import calculate_activity_calories_burnt
 
 
 def last_parsed_activity():
@@ -24,16 +25,18 @@ def get_users_from_activities(activities):
 def get_user_goals(start_time, end_time):
     pass
 
+
 def main():
     with app.app_context():
-        activities = Activity.get_activities_since(last_parsed_activity())
-        if not activities:
-            exit()
-        write_last_parsed_activity(activities[len(activities)-1].get('id'))
-        first_activity_time = activities[0].get('start_datetime')
-        last_activity_time = activities[len(activities)-1].get('start_datetime')
-        users = get_users_from_activities(activities)
-        print users
+        # activities = Activity.get_activities_since(last_parsed_activity())
+        calculate_activity_calories_burnt(last_parsed_activity())
+        # if not activities:
+        #     exit()
+        # write_last_parsed_activity(activities[len(activities)-1].get('id'))
+        # first_activity_time = activities[0].get('start_datetime')
+        # last_activity_time = activities[len(activities)-1].get('start_datetime')
+        # users = get_users_from_activities(activities)
+        # print users
         # for activity in activities:
         #     print "test"
         # print last_parsed_activity()
