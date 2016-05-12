e47b20395583ad8793004c54461e1345dba6e514
jainikvora <jainik.vora231@gmail.com>
2016-05-11 20:27:53 -0700

    fix

--- app/models/analytics.py
+++ app/models/analytics.py
@@ -14,11 +14,11 @@ class Analytics(object):
                   SUM(distance) distance,
                   TRUNCATE(SUM(calories_burnt),2) calories,
                   HOUR(TIME(TRUNCATE(FROM_UNIXTIME(start_datetime/1000),0))) dayhour FROM t_user_activity
-                WHERE user_id = '%s'
-                AND DATE_FORMAT(FROM_UNIXTIME(start_datetime/1000), '%Y-%m-%d') = DATE_FORMAT(CURDATE(), '%Y-%m-%d')
+                WHERE user_id = '{}'
+                AND DATE_FORMAT(FROM_UNIXTIME(start_datetime/1000), "%Y-%m-%d") = DATE_FORMAT(CURDATE(), "%Y-%m-%d")
                 GROUP BY dayhour
                 """
-        result = {row['dayhour']: {'calories': row['calories'], 'distance': float(row['distance'])} for row in Db.execute_select_query(query % user_id)}
+        result = {row['dayhour']: {'calories': row['calories'], 'distance': float(row['distance'])} for row in Db.execute_select_query(query.format(user_id))}
         calories = []
         distance = []
         for hour in HOURS:
